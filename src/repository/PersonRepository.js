6cc95b2350d523d06210127754cfe6c2c9b2e632
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-11-02 14:02:04 -0300

    person refactored async await

--- src/repository/PersonRepository.js
+++ src/repository/PersonRepository.js
@@ -1,103 +1,67 @@
 const BaseRepository = require('./BaseRepository');
 
 class PersonRepository extends BaseRepository {
-  constructor(errorCallback) {
-    super();
-
-    this.connection.connect((err) => {
-      if (err && typeof errorCallback === 'function') errorCallback(err);
-    });
-  }
-
-  create(person) {
-    return new Promise((resolve, reject) => {
+  async create(person) {
+    try {
       const query =
         // eslint-disable-next-line max-len
         'INSERT INTO pessoa (nome, data_nascimento, cpf, rg, telefone1, telefone2, id_endereco, estado_civil) VALUES (?, ?, ?, ?, ?, ?, ?, ?);';
 
-      this.connection.query(
-        query,
-        [
-          person.nome,
-          person.dataNascimento,
-          person.cpf,
-          person.rg,
-          person.telefone1,
-          person.telefone2,
-          person.idEndereco,
-          person.estadoCivil,
-        ],
-        (error, result) => {
-          if (error) {
-            return reject(
-              new Error(`Ocorreu um erro ao gravar os dados: ${error}`),
-            );
-          }
-          return resolve(result.insertId);
-        },
-      );
-    });
+      const result = await this.query(query, [
+        person.nome,
+        person.dataNascimento,
+        person.cpf,
+        person.rg,
+        person.telefone1,
+        person.telefone2,
+        person.idEndereco,
+        person.estadoCivil,
+      ]);
+      return result.insertId;
+    } catch (error) {
+      return error;
+    }
   }
 
-  getById(id) {
-    return new Promise((resolve, reject) => {
+  async getById(id) {
+    try {
       const query = 'SELECT * FROM pessoa WHERE id = ?;';
-
-      this.connection.query(query, [id], (error, result) => {
-        if (error) {
-          return reject(
-            new Error(`Ocorreu um erro ao obter os dados: ${error}`),
-          );
-        }
-        return resolve(result);
-      });
-    });
+      const result = await this.query(query, [id]);
+      return result;
+    } catch (error) {
+      return error;
+    }
   }
 
-  update(person) {
-    return new Promise((resolve, reject) => {
+  async update(person) {
+    try {
       const query =
         // eslint-disable-next-line max-len
         'UPDATE pessoa SET nome = ?, data_nascimento = ?, cpf = ?, rg = ?, telefone1 = ?, telefone2 = ?, id_endereco = ?, estado_civil = ? WHERE id = ?;';
-
-      this.connection.query(
-        query,
-        [
-          person.nome,
-          person.dataNascimento,
-          person.cpf,
-          person.rg,
-          person.telefone1,
-          person.telefone2,
-          person.estadoCivil,
-          person.id,
-        ],
-        (error, result) => {
-          if (error) {
-            return reject(
-              new Error(`Ocorreu um erro ao atualizar os dados: ${error}`),
-            );
-          }
-          return resolve(result);
-        },
-      );
-    });
+      const result = await this.query(query, [
+        person.nome,
+        person.dataNascimento,
+        person.cpf,
+        person.rg,
+        person.telefone1,
+        person.telefone2,
+        person.estadoCivil,
+        person.id,
+      ]);
+      return result;
+    } catch (error) {
+      return error;
+    }
   }
 
-  deletePerson(id) {
-    return new Promise((resolve, reject) => {
+  async deletePerson(id) {
+    try {
       const query = 'DELETE FROM pessoa WHERE id = ?';
-
-      this.connection.query(query, [id], (error, result) => {
-        if (error) {
-          return reject(
-            new Error(`Ocorreu um erro ao excluir os dados: ${error}`),
-          );
-        }
-
-        return resolve(result && result.length > 0);
-      });
-    });
+      const result = await this.query(query, [id]);
+      return result;
+    } catch (error) {
+      return error;
+    }
   }
 }
 
