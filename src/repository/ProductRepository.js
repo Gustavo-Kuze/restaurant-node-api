76f0a5a1aeb9f9737f2ac8545f5117a4ea0388a6
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-11-02 13:56:58 -0300

    product refactored async await

--- src/repository/ProductRepository.js
+++ src/repository/ProductRepository.js
@@ -1,122 +1,80 @@
 const BaseRepository = require('./BaseRepository');
 
 class ProductRepository extends BaseRepository {
-  constructor(errorCallback) {
-    super();
-
-    this.connection.connect((err) => {
-      if (err && typeof errorCallback === 'function') errorCallback(err);
-    });
-  }
-
-  create(product) {
-    return new Promise((resolve, reject) => {
+  async create(product) {
+    try {
       const query =
         // eslint-disable-next-line max-len
         'INSERT INTO produto (preco, nome, descricao, tamanho) VALUES (?, ?, ?, ?);';
-
-      this.connection.query(
-        query,
-        [product.preco, product.nome, product.descricao, product.tamanho],
-        (error, result) => {
-          if (error) {
-            return reject(
-              new Error(`Ocorreu um erro ao gravar os dados: ${error}`),
-            );
-          }
-          return resolve(result.insertId);
-        },
-      );
-    });
+      const result = await this.query(query, [
+        product.preco,
+        product.nome,
+        product.descricao,
+        product.tamanho,
+      ]);
+      return result.insertId;
+    } catch (error) {
+      return error;
+    }
   }
 
-  getById(id) {
-    return new Promise((resolve, reject) => {
+  async getById(id) {
+    try {
       const query = 'SELECT * FROM produto WHERE id = ? AND desabilitado = 0;';
-
-      this.connection.query(query, [id], (error, result) => {
-        if (error) {
-          return reject(
-            new Error(`Ocorreu um erro ao obter os dados: ${error}`),
-          );
-        }
-        return resolve(result);
-      });
-    });
+      const result = await this.query(query, [id]);
+      return result;
+    } catch (error) {
+      return error;
+    }
   }
 
-  getAllEnabled() {
-    return new Promise((resolve, reject) => {
+  async getAllEnabled() {
+    try {
       const query = 'SELECT * FROM produto WHERE desabilitado = 0;';
-
-      this.connection.query(query, (error, result) => {
-        if (error) {
-          return reject(
-            new Error(`Ocorreu um erro ao obter os dados: ${error}`),
-          );
-        }
-        return resolve(result);
-      });
-    });
+      const result = await this.query(query);
+      return result;
+    } catch (error) {
+      return error;
+    }
   }
 
-  getAll() {
-    return new Promise((resolve, reject) => {
+  async getAll() {
+    try {
       const query = 'SELECT * FROM produto;';
-
-      this.connection.query(query, (error, result) => {
-        if (error) {
-          return reject(
-            new Error(`Ocorreu um erro ao obter os dados: ${error}`),
-          );
-        }
-        return resolve(result);
-      });
-    });
+      const result = await this.query(query);
+      return result;
+    } catch (error) {
+      return error;
+    }
   }
 
-  update(product) {
-    return new Promise((resolve, reject) => {
+  async update(product) {
+    try {
       const query =
         // eslint-disable-next-line max-len
         'UPDATE produto SET preco = ?, nome = ?, descricao = ?, tamanho = ?, desabilitado = ? WHERE id = ?;';
-
-      this.connection.query(
-        query,
-        [
-          product.preco,
-          product.nome,
-          product.descricao,
-          product.tamanho,
-          product.desabilitado || 0,
-          product.id,
-        ],
-        (error, result) => {
-          if (error) {
-            return reject(
-              new Error(`Ocorreu um erro ao atualizar os dados: ${error}`),
-            );
-          }
-          return resolve(result);
-        },
-      );
-    });
+      const result = await this.query(query, [
+        product.preco,
+        product.nome,
+        product.descricao,
+        product.tamanho,
+        product.desabilitado || 0,
+        product.id,
+      ]);
+      return result;
+    } catch (error) {
+      return error;
+    }
   }
 
-  deleteProduct(id) {
-    return new Promise((resolve, reject) => {
+  async deleteProduct(id) {
+    try {
       const query = 'UPDATE produto SET desabilitado = 1 WHERE id = ?;';
-
-      this.connection.query(query, [id], (error, result) => {
-        if (error) {
-          return reject(
-            new Error(`Ocorreu um erro ao excluir os dados: ${error}`),
-          );
-        }
-
-        return resolve(result && result.length > 0);
-      });
-    });
+      const result = await this.query(query, [id]);
+      return result;
+    } catch (error) {
+      return error;
+    }
   }
 }
 
