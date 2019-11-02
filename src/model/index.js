f7a84a0cd1b67bab90ed69b403eb9489af922645
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-11-02 10:08:01 -0300

    productModels

--- src/model/index.js
+++ src/model/index.js
@@ -1,6 +1,7 @@
 const userModels = require('./user');
 const addressModels = require('./address');
 const personModels = require('./person');
+const productModels = require('./product');
 
 const getModel = (url) => {
   if (url.includes('/api/usuario/registrar')) return userModels.register;
@@ -10,6 +11,9 @@ const getModel = (url) => {
   }
   if (url.includes('/api/informacoes-pessoais')) {
     return personModels.createOrUpdatePerson;
+  }
+  if (url.includes('/api/produto')) {
+    return productModels.createOrUpdateProduct;
   }
   return '';
 };
---
+++ src/model/product.js
@@ -0,0 +1,22 @@
+const joi = require('@hapi/joi');
+
+const createOrUpdateProduct = joi.object({
+  preco: joi.number().required(),
+  nome: joi
+    .string()
+    .min(5)
+    .max(45)
+    .required(),
+  descricao: joi
+    .string()
+    .min(5)
+    .max(100)
+    .required(),
+  tamanho: joi
+    .string()
+    .min(1)
+    .max(10)
+    .required(),
+});
+
+module.exports = { createOrUpdateProduct };
