dde3470a6301f053b96e19cad796bf82415879db
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-11-02 09:49:28 -0300

    productRepository

---
+++ src/repository/ProductRepository.js
@@ -0,0 +1,92 @@
+const BaseRepository = require('./BaseRepository');
+
+class ProductRepository extends BaseRepository {
+  constructor(errorCallback) {
+    super();
+
+    this.connection.connect((err) => {
+      if (err && typeof errorCallback === 'function') errorCallback(err);
+    });
+  }
+
+  create(product) {
+    return new Promise((resolve, reject) => {
+      const query =
+        // eslint-disable-next-line max-len
+        'INSERT INTO produto (preco, nome, descricao, tamanho) VALUES (?, ?, ?, ?);';
+
+      this.connection.query(
+        query,
+        [product.preco, product.nome, product.descricao, product.tamanho],
+        (error, result) => {
+          if (error) {
+            return reject(
+              new Error(`Ocorreu um erro ao gravar os dados: ${error}`),
+            );
+          }
+          return resolve(result.insertId);
+        },
+      );
+    });
+  }
+
+  getById(id) {
+    return new Promise((resolve, reject) => {
+      const query = 'SELECT * FROM produto WHERE id = ?;';
+
+      this.connection.query(query, [id], (error, result) => {
+        if (error) {
+          return reject(
+            new Error(`Ocorreu um erro ao obter os dados: ${error}`),
+          );
+        }
+        return resolve(result);
+      });
+    });
+  }
+
+  update(product) {
+    return new Promise((resolve, reject) => {
+      const query =
+        // eslint-disable-next-line max-len
+        'UPDATE produto SET preco = ?, nome = ?, descricao = ?, tamanho = ? WHERE id = ?;';
+
+      this.connection.query(
+        query,
+        [
+          product.preco,
+          product.nome,
+          product.descricao,
+          product.tamanho,
+          product.id,
+        ],
+        (error, result) => {
+          if (error) {
+            return reject(
+              new Error(`Ocorreu um erro ao atualizar os dados: ${error}`),
+            );
+          }
+          return resolve(result);
+        },
+      );
+    });
+  }
+
+  deleteProduct(id) {
+    return new Promise((resolve, reject) => {
+      const query = 'UPDATE produto SET desabilitado = 1 WHERE id = ?;';
+
+      this.connection.query(query, [id], (error, result) => {
+        if (error) {
+          return reject(
+            new Error(`Ocorreu um erro ao excluir os dados: ${error}`),
+          );
+        }
+
+        return resolve(result && result.length > 0);
+      });
+    });
+  }
+}
+
+module.exports = ProductRepository;
